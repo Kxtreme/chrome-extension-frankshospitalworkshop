3db269db2eb914bb05dae2887ee02c7662d17fd2
Tiago <tiagodabatalha@hotmail.com>
2020-04-27 12:51:00 +0100

    replace for search button

--- content.js
+++ content.js
@@ -1,4 +1,12 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+var spans = document.getElementsByTagName('span');
+for (var i = 0, l = spans.length; i < l; i++) {
+    var span = spans[i];
+  if(span.innerText.includes('Download prohibited'))
+  var a = span.previousElementSibling;
+  if(a) {
+      span.innerText = '';
+      const aParts = a.href.split('/');
+      a.href = 'https://www.google.com/search?q=' + aParts[aParts.length - 1]
+      a.innerText = 'Search'
+  }
 }
